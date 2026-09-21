d0eb9e34b42ef047a141551f2a5285d9075996ed
9reis <lucasreis_9@hotmail.com>
2026-09-21 13:45:59 -0300

    Altera os estágios do jogo

--- secretword/src/components/StartScreen.js
+++ secretword/src/components/StartScreen.js
@@ -2,12 +2,12 @@ import React from 'react'
 /*Import para arq na msm pasta*/
 import "./StartScreen.css"
 
-const StartScreen = () => {
+const StartScreen = ({ startGame }) => {
     return (
         <div className='start'>
             <h1>Secret Word</h1>
             <p>Clique no botão abaixo para começar a jogar</p>
-            <button>Começar a jogar</button>
+            <button onClick={startGame}>Começar a jogar</button>
         </div>
     )
 }
