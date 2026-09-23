b5faba50a562df507139baa22cebc4bbab531421
9reis <lucasreis_9@hotmail.com>
2026-09-22 23:45:50 -0300

    Condicao de vitória

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -32,7 +32,7 @@ function App() {
   const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
-  const pickWordAndCategory = () => {
+  const pickWordAndCategory = useCallback(() => {
     // Pick random category
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
@@ -44,10 +44,13 @@ function App() {
 
     // Retorna como obj, já que está sendo desestruturado como objeto(abaixo)
     return { word, category };
-  }
+  }, [words]);
 
   // Starts the secret word game  
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    // Clear all letters
+    clearLetterStates();
+
     // Pick word and pick category
     // Desestrutura o retorno da função 
     const { word, category } = pickWordAndCategory();
@@ -64,7 +67,10 @@ function App() {
 
     // Primeiro deve estar tudo pronto, antes de iniciar o game;
     setGameStage(stages[1].name);
-  };
+
+    // O useCallback
+    // Recebe algo que é fundamental para o funcionamento 
+  }, [pickWordAndCategory]);
 
   // Process the letter input 
   const verifyLetter = (letter) => {
@@ -104,6 +110,8 @@ function App() {
     setWrongLetters([]);
   }
 
+  // Check if guesses ended
+
   // Monitora um dado, executa algo sempre que ele atualizar
   useEffect(() => {
     if (guesses <= 0) {
@@ -112,6 +120,21 @@ function App() {
       setGameStage(stages[2].name);
     }
   }, [guesses]);
+
+  // Check win condition
+  useEffect(() => {
+    // Cria uma lista de letras únicas
+    const uniqueLetters = [...new Set(letters)]
+
+    // Win condition 
+    if (guessedLetters.length === uniqueLetters.length) {
+      //add score
+      setScore((actualscore) => actualscore += 100)
+
+      //restart game with new word
+      startGame();
+    }
+  }, [guessedLetters, letters, startGame]);
 
   // Restarts the game  
   const retry = () => {
